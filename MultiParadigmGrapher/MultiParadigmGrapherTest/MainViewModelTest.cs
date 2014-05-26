69df2302d299444021d0b6701a2d3fe69194a57b
LasseBP <lasse@huhej.dk>
2014-05-26 17:14:49 +0200

    Tests tilfoejet

---
+++ MultiParadigmGrapher/MultiParadigmGrapherTest/MainViewModelTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiParadigmGrapher.ViewModel;
+
+namespace MultiParadigmGrapherTest
+{
+    [TestClass]
+    public class MainViewModelTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //var vm = new MainViewModel(new ContentHandling.ContentProvider());
+        }
+    }
+}
